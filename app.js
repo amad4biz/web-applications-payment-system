bbea60a1adf6bb4b8c3f029b565eca9edc82f80d
Thilo Ilg <contact@thiloilg.com>
2016-10-12 19:38:29 +0200

    jquery, bootstrap and basic html added

--- app.js
+++ app.js
@@ -2,4 +2,28 @@
 * Single web page application based on express.js
 */
 
-console.log('hey hey');
+console.log('running ...');
+
+var express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    publicDirectory = path.join(__dirname, '/public');
+
+// logging middleware
+app.use(morgan('dev'));
+
+app.use(express.static(publicDirectory));
+
+//app.use(bodyParser.json());
+
+// own json api
+app.get('/json-api', function(req, res){
+    res.send({success: true, api: "json-api"});
+})
+
+var server = app.listen(port, '127.0.0.1', function(){
+    console.log('server started on port %s', port);
+});
